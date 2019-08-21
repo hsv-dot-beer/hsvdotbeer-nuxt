84bc73297054590c0ff22dc163cf6c976b26e681
Michael Carroll <michael@openrobotics.org>
2019-08-21 16:00:48 -0500

    Some form of infinite scroll.

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,9 @@
             <v-card-text class="align-end"></v-card-text>
           </v-card>
         </v-col>
+        <div v-if="next">
+          <v-progress-circular indeterminate color="primary" class="bottom" />
+        </div>
       </div>
     </v-flex>
   </v-layout>
@@ -20,14 +23,43 @@ import { mapState } from 'vuex'
 
 export default {
   components: {},
+  data() {
+    return {
+      bottom: false
+    }
+  },
   computed: {
     ...mapState({
+      next: (state) => state.beers.links.next,
       count: (state) => state.beers.count,
-      beers: (state) => state.beers.list
+      beers: (state) => state.beers.records
     })
   },
+  watch: {
+    bottom(bottom) {
+      if (bottom) {
+        this.$store.dispatch('beers/loadNextPage')
+      }
+    }
+  },
   async fetch({ store }) {
-    await store.dispatch('beers/getActive')
+    await store.dispatch('beers/loadPage', {
+      options: { on_tap: true, o: 'name' }
+    })
+  },
+  mounted() {
+    window.addEventListener('scroll', () => {
+      this.bottom = this.bottomVisible()
+    })
+  },
+  methods: {
+    bottomVisible() {
+      const scrollY = window.scrollY
+      const visible = document.documentElement.clientHeight
+      const pageHeight = document.documentElement.scrollHeight
+      const bottomOfPage = visible + scrollY >= pageHeight
+      return bottomOfPage || pageHeight < visible
+    }
   }
 }
 </script>
--- store/beers.js
+++ store/beers.js
@@ -1,17 +1,105 @@
-export const state = () => ({
-  list: [],
-  count: 0
-})
+const STATUS_INITIAL = 'INITIAL'
+const STATUS_LOADING = 'LOADING'
+const STATUS_ERROR = 'ERROR'
+const STATUS_SUCCESS = 'SUCCESS'
 
-export const mutations = {
-  set(state, beers) {
-    console.log('set')
-    state.list = beers.results
-    state.count = beers.count
+const storeRecord = (records) => (newRecord) => {
+  const existingRecord = records.find((r) => r.id === newRecord.id)
+
+  if (existingRecord) {
+    Object.assign(existingRecord, newRecord)
+  } else {
+    records.push(newRecord)
   }
 }
 
+const handleError = (commit) => (errorResponse) => {
+  commit('SET_STATUS', STATUS_ERROR)
+  commit('STORE_ERROR', errorResponse)
+  throw errorResponse
+}
+
+const getOptionsQuery = (optionsObject = {}) =>
+  Object.keys(optionsObject)
+    .filter((k) => typeof optionsObject[k] !== 'undefined')
+    .map((k) => `${k}=${encodeURIComponent(optionsObject[k])}`)
+    .join('&')
+
+export const state = () => ({
+  error: null,
+  status: STATUS_INITIAL,
+  links: {},
+  count: 0,
+  records: []
+})
+
+export const mutations = {
+  SET_STATUS: (state, status) => {
+    state.status = status
+  },
+  STORE_ERROR: (state, error) => {
+    state.error = error
+  },
+  STORE_LINKS: (state, links) => {
+    state.count = links.count
+    state.links = links
+  },
+  STORE_RECORD: (state, newRecord) => {
+    const { records } = state
+    storeRecord(records)(newRecord)
+  },
+  STORE_RECORDS: (state, newRecords) => {
+    const { records } = state
+    newRecords.forEach(storeRecord(records))
+  }
+}
+
+export const getters = {
+  isLoading: (state) => state.status === STATUS_LOADING,
+  isError: (state) => state.status === STATUS_ERROR,
+  error: (state) => state.error,
+  hasPrevious: (state) => !!state.links.prev,
+  hasNext: (state) => !!state.links.next,
+  all: (state) => state.records,
+  byId: (state) => ({ id }) => state.records.find((r) => r.id === id)
+}
+
 export const actions = {
+  async loadPage({ commit }, { options }) {
+    commit('SET_STATUS', STATUS_LOADING)
+    const url = `beers/?${getOptionsQuery(options)}`
+    console.log(url)
+    await this.$axios
+      .get(url)
+      .then((res) => {
+        if (res.status === 200) {
+          commit('SET_STATUS', STATUS_SUCCESS)
+          commit('STORE_RECORDS', res.data.results)
+          commit('STORE_LINKS', {
+            next: res.data.next,
+            prev: res.data.prev,
+            count: res.data.count
+          })
+        }
+      })
+      .catch(handleError(commit))
+  },
+  async loadNextPage({ commit, state }) {
+    await this.$axios
+      .get(state.links.next)
+      .then((res) => {
+        if (res.status === 200) {
+          commit('SET_STATUS', STATUS_SUCCESS)
+          commit('STORE_RECORDS', res.data.results)
+          commit('STORE_LINKS', {
+            next: res.data.next,
+            prev: res.data.prev,
+            count: res.data.count
+          })
+        }
+      })
+      .catch(handleError(commit))
+  },
   async getActive({ commit }) {
     await this.$axios.get('beers/?on_tap=true').then((res) => {
       if (res.status === 200) {
