2e89f428db7aa9aa0050121c1ca8f80eae9ec839
Michael Carroll <michael@hsv.beer>
2019-09-06 20:53:49 -0500

    Sort widget

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,8 @@
         <span class="beer-total">{{ beerCount }}</span>
         <span class="d-none">matching</span> beers on tap
       </h2>
+      <sort-widget @updated="updateBeers($event)" />
     </div>
-
     <beer-list />
   </div>
 </template>
@@ -14,10 +14,12 @@
 <script>
 import { mapState } from 'vuex'
 import BeerList from '~/components/BeerList'
+import SortWidget from '~/components/SortWidget'
 
 export default {
   components: {
-    BeerList
+    BeerList,
+    SortWidget
   },
   computed: {
     ...mapState({
@@ -28,6 +30,32 @@ export default {
     await store.dispatch('beers/loadPage', {
       options: { on_tap: true, o: 'name' }
     })
+  },
+  methods: {
+    updateBeers ({ sort, invert }) {
+      let ordering = ''
+
+      if (sort === 'name') {
+        ordering = 'name'
+      } else if (sort === 'brewer') {
+        ordering = 'manufacturer__name'
+      } else if (sort === 'style') {
+        ordering = 'style__name'
+      } else if (sort === 'abv') {
+        ordering = 'abv'
+      } else {
+        // Default to name
+        ordering = 'name'
+      }
+
+      if (invert) {
+        ordering = `-${ordering}`
+      }
+
+      this.$store.dispatch('beers/loadPage', {
+        options: { on_tap: true, o: ordering }
+      })
+    }
   },
   beforeRouteLeave (to, from, next) {
     this.$store.commit('HIDE_MODAL')
